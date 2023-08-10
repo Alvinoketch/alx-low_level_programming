1c15823b02f6fa3ca8e9d7d3f4022f1622e1aee2
Alvinoketch <oketchalvin28@gmail.com>
2023-08-10 13:16:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *main-entry point
- *description: prints using printf function
- *Return: 0 for success
+ * main-entry point
+ * description: prints using printf function
+ * Return: 0 for success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
